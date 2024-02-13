95131bf7b839febc0e28bffc81354866cc5920f1
Freddy Carias <freddycarias@Freddys-MacBook-Pro.local>
2024-02-13 16:55:44 -0600

    prueba 5

--- Vacaciones/View/DayView.swift
+++ Vacaciones/View/DayView.swift
@@ -55,35 +55,3 @@ struct DayView_Previews: PreviewProvider {
         DayView(typeData: "day")
     }
 }
-
-
-
-//private var contacts: [ContactModel] = [
-//    .init(name: "Mario", phoneNumber: "4356 3413", email: "mario@gmail.com", bankDestination: "Banco Cuscatlán"),
-//    .init(name: "Maria", phoneNumber: "3562 4623", email: "maria@gmail.com", bankDestination: "Banco Cuscatlán"),
-//    .init(name: "Marta", phoneNumber: "9485 8484", email: "marta@gmail.com", bankDestination: "Banco Cuscatlán"),
-//    .init(name: "Marlon", phoneNumber: "9385 8393", email: "marlon@gmail.com", bankDestination: "Banco Cuscatlán"),
-//    .init(name: "Aron", phoneNumber: "9394 8584", email: "aron@gmail.com", bankDestination: "Banco Cuscatlán"),
-//    .init(name: "Diago", phoneNumber: "9398 5824", email: "diago@gmail.com", bankDestination: "Banco Cuscatlán"),
-//]
-//
-//var filteredContacts: [ContactModel] {
-//    guard !searchTerm.isEmpty else { return contacts.sorted { $0.name < $1.name } }
-//    return contacts.filter { $0.name.localizedCaseInsensitiveContains(searchTerm) }
-//}
-//
-//var groupedContacts: [String: [ContactModel]] {
-//    Dictionary(grouping: filteredContacts, by: { String($0.name.prefix(1)) })
-//}
-//
-//List {
-//    ForEach(groupedContacts.keys.sorted(), id: \.self) { key in
-//        Section(header: Text(key)) {
-//            ForEach(groupedContacts[key]!, id: \.id) { contact in
-//                NavigationLink(destination: AddNewContact(bankDestinationContact: contact.bankDestination, nameContact: contact.name, phoneNomberContact: contact.phoneNumber, emailContact: contact.email)){
-//                    Text(contact.name)
-//                }
-//            }
-//        }
-//    }
-//}
